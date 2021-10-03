8939214d6523a629197b2035b8c3e4a5f7a93c9c
Nando San Miguel <mythemc@gmail.com>
2021-10-02 21:38:37 -0500

    Anadir conector DB y app.php

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "34.136.42.90";
+$username = "root";
+$password = "nrPfAnGNbChhKu02";
+$dbname = "data";
+
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+?>
